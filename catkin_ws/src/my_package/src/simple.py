e6f8c84cd854626918ed97f8640c1533d3f03b5e
marcos daniel santana <85710388+marcospontoexe@users.noreply.github.com>
2024-05-10 22:51:23 -0300

    Update simple.py

--- catkin_ws/src/my_package/src/simple.py
+++ catkin_ws/src/my_package/src/simple.py
@@ -6,4 +6,10 @@ import rospy # Import the rospy, which is a Python library for ROS.
 
 rospy.init_node('ObiWan') # Initiate a node called ObiWan
 
-print("Help me Obi-Wan Kenobi, you're my only hope") # A simple Python print
+rate = rospy.Rate(2)               # We create a Rate object of 2Hz
+while not rospy.is_shutdown():     # Endless loop until Ctrl + C
+   print("Help me Obi-Wan Kenobi, you're my only hope")
+   rate.sleep()                    # We sleep the needed time to maintain the Rate fixed above
+    
+# This program creates an endless loop that repeats itself 2 times per second (2Hz) until somebody presses Ctrl + C
+# in the Shell
